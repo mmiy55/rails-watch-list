25bac0738190f087b945eea2aab652345f2656f5
Miriam Fiandanese <mmiy55@gmail.com>
2023-02-17 16:33:20 +0900

    cloudinary setup and model setup

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,7 @@ class List < ApplicationRecord
   # associations
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
   # validations
   validates :name, presence: true, uniqueness: true
 end
